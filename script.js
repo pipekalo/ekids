1fbeb1c7e860127c01ab7f6c2c3b9d5b74897c26
Oleksii Polishchuk <Oleksii_Polishchuk@epam.com>
2019-05-04 11:23:28 +0300

    Variable creating

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
- 
+(function (){
+    let tasks = {
+        current: [{
+            taskId: doId(),
+            taskContent: "Task 1",
+            taskState: "current"
+        },{
+            taskId: doId(),
+            taskContent: "Task 1",
+            taskState: "current"
+        }],
+        done:[{
+            taskId: doId(),
+            taskContent: "Task 1",
+            taskState: "current"
+        }],
+        get allTasks(){
+            return this.current.length + this.done.length
+        },
+        get doneTasks(){
+            return this.done.length
+        }
+    },
+    taskList = document.getElementById("app__list"),
+    allTasks = document.getElementById("js-all-tasks"),
+    doneTasks = document.getElementById("js-done-tasks"),
+    addNewTaskField = document.getElementById("app__task-new");
+})
